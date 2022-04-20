2f5cdd215088970d6efec4d73a2b6231a53e32c6
ho4ole <daniilkuznetss@gmail.com>
2022-04-20 03:55:50 +0200

    test deploy

--- server/index.js
+++ server/index.js
@@ -53,9 +53,9 @@ app.get("/users", async (request, response) => {
     }
 });
 
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});*/
+});
 
 app.listen(PORT, () => {
     console.log(`Server up at ${PORT}`)
